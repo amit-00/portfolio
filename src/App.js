31ada91d54a20a18e6e409af0b805c358281500f
Amit Verma <amit.v@hotmail.com>
2020-10-29 16:54:04 -0400

    initialized react app and did cleanup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Route>
+      <div className="App">
+        <Switch>
+          
+        </Switch>
+      </div>
+    </Route>
+  );
+}
+
+export default App;
